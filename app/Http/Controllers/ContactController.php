91b9b6995e48741bbb83aee5a3eda0540dd65565
hansen1416 <L.Han2@uni.brighton.ac.uk>
2026-01-22 18:16:04 +0000

    1

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ContactController extends Controller
+{
+    public function store(Request $request)
+    {
+        // TODO auth: resolve current user_id
+        // $userId = $request->user()->id;
+        $userId = 1;
+
+        $data = $request->validate([
+            'email' => [
+                'required',
+                'email',
+                'max:255',
+                Rule::unique('contacts', 'email')->where('user_id', $userId),
+            ],
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+        ]);
+
+        $contact = Contact::create([
+            'user_id' => $userId,
+            'email' => $data['email'],
+            'name' => $data['name'],
+            'description' => $data['description'] ?? null,
+        ]);
+
+        return response()->json(['id' => $contact->id], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EmailCampaignController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('ping', fn () => response()->json(['ok' => true]));
@@ -9,3 +10,4 @@ Route::post('/email-campaigns', [EmailCampaignController::class, 'store']);
 Route::get('/email-campaigns/{email_campaign}/graph', [EmailCampaignController::class, 'getGraph']);
 Route::put('/email-campaigns/{email_campaign}/graph', [EmailCampaignController::class, 'updateGraph']);
 
+Route::post('/contacts', [ContactController::class, 'store']);
